e54ee2547f929ad6b6415e22f04549852e7aa727
spartam <kwpardon@vub.ac.be>
2016-08-05 00:23:34 +0200

    annoying warnings WERE annoying, now they're gone

--- src/solution2.pl
+++ src/solution2.pl
@@ -31,14 +31,14 @@ is_conflict_free_pair(Event1, Event2) :-	is_valid_event(Event1),
 											EID1 \= EID2,
 											conflict_free_events(Event1, Event2).
 
-conflict_free_events(event(EID1, RID1, Day1, Start1), event(EID2, RID2, Day2, Start2)) :- 	Day1 \= Day2,!. %% both valid events on a different day == no conflicts
-conflict_free_events(event(EID1, RID1, Day1, Start1), event(EID2, RID2, Day2, Start2)) :- 	duration(EID1, Duration1), %% on the same day but on timeslot overlap
+conflict_free_events(event(_, _, Day1, _), event(_, _, Day2, _)) :- 	Day1 \= Day2,!. %% both valid events on a different day == no conflicts
+conflict_free_events(event(EID1, _, _, Start1), event(EID2, _, _, Start2)) :- 	duration(EID1, Duration1), %% on the same day but on timeslot overlap
 																							End1 is Start1 + Duration1 - 1,
 																							not(between(Start1, End1, Start2)),
 																							duration(EID2, Duration2),
 																							End2 is Start2 + Duration2 - 1,
 																							not(between(Start2, End2, Start1)),!.
-conflict_free_events(event(EID1, RID1, Day1, Start1), event(EID2, RID2, Day2, Start2)) :- 	RID1 \= RID2,	%% Same day, overlapping times, differen room, different teacher
+conflict_free_events(event(EID1, RID1, _, _), event(EID2, RID2, _, _)) :- 	RID1 \= RID2,	%% Same day, overlapping times, differen room, different teacher
 																							has_exam(C1, EID1),
 																							teaches(L1, C1),
 																							has_exam(C2, EID2),
